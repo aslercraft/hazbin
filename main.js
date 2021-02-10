16a8e1c2b226a41932c4073fd6efe2bb80227e36
aslercraft <75515091+aslercraft@users.noreply.github.com>
2021-02-09 19:46:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.Baner');
+ScrollReveal().reveal('.menu');
+ScrollReveal().reveal('.promo');
+ScrollReveal().reveal('.video');
+ScrollReveal().reveal('.info');
+ScrollReveal().reveal('.music');
